a03722ccd3cc3234a5461a7d9785385e5949f8d0
Kyle Hickey <kylehickey15@gmail.com>
2023-07-07 17:04:17 -0500

    Added bluesky integration

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-#import twitter
 import tweepy
+from atproto import Client
+import pytumblr2
 import config
 import time
 import forge
@@ -7,11 +8,10 @@ import forge
 #Set the script configuration
 about = config.getJSON('about.json')
 flags = config.getJSON("flags.json")
-twitter_credentials = config.getJSON("credentials/twitter.json")#previously had ../../ as a path prefix. Not sure why. This might be important
+twitter_credentials = config.getJSON("../../credentials/twitter.json")
+bsky_credentials = config.getJSON("../../credentials/bluesky.json")
+tumblr_credentials = config.getJSON("../../credentials/tumblr.json")
 
-#Posts a tweet
-def tweet(body):
-    newTweet = api.PostUpdate(body)
 
 #Initializes twitter api and posts, if not disabled
 if not config.isDisabled(flags['twitter']):
@@ -20,11 +20,30 @@ if not config.isDisabled(flags['twitter']):
         consumer_secret=twitter_credentials["consumer_secret"],
         access_token=twitter_credentials["access_token_key"],
         access_token_secret=twitter_credentials["access_token_secret"])
+
     #Post the tweet
-
     response = client.create_tweet(text=forge.item())
+    print(response)
 
-print(response)
+#Initializes bluesky api and posts, if not disabled
+if not config.isDisabled(flags['bluesky']):
+    #Initialize the bluesky AT Protocol
+    client = Client()
+    client.login(bsky_credentials["bsky_username"], bsky_credentials["bsky_app_password"])
+
+    #Post the skeet
+    response = client.send_post(text=forge.item())
+    print(response)
+
+#Initializes the Tumblr API, if not disabled
+if not config.isDisabled(flags['tumblr']):
+    #Initialize the tumblr api
+    client = pytumblr2.TumblrRestClient(
+    tumblr_credentials["consumer_key"], tumblr_credentials["consumer_secret"], tumblr_credentials["oauth_token"], tumblr_credentials["oauth_secret"])
+
+    #Post the ... post
+    response = client.create_post("TES-ItemsBot", content=[{'type': 'text', 'text': forge.item()}])
+    print(response)
 
 ##############################Debug##################################
 def timed_test(seconds):
@@ -41,6 +60,3 @@ def timed_test(seconds):
         if elapsed_time > seconds:
             print("Test completed. " + str(iterations) + " items generated in " + str(seconds) + " second(s).")
             break
-
-#tweet(forge.item())
-#timed_test(1)
