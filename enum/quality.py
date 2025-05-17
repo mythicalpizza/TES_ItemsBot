8be8565ad11f86e711873e82a2bc109903392647
Kyle Hickey <kylehickey15@gmail.com>
2025-05-16 22:21:27 -0500

    Enums bitch
    Muahahahaha

---
+++ enum/quality.py
@@ -0,0 +1,24 @@
+from enum import enum
+
+class Quality(Enum):
+    ANCIENT = 1
+    AVERAGE = 2
+    BATTLESCARRED = 3
+    BEAUTIFUL = 4
+    COMMON = 5
+    CUNTY = 6
+    DUCTTAPE = 7
+    EPIC = 8
+    EXPENSIVE = 9
+    EXQUISITE = 10
+    EXTRAVAGANT = 11
+    FINE = 12
+    FLAWLESS = 13
+    HIGHCLASS = 14
+    LEGENDARY = 15
+    NICE = 16
+    ORDINARY = 17
+    STUPID = 18
+    SUPERIOR = 19
+    TATTERED = 20
+    WORN = 21
